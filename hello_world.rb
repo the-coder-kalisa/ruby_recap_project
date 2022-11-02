f30685c576bbb0ee9f59e9afcc56b01120714d4f
the-coder-kalisa <gthecoderkalisaineza@gmail.com>
2022-11-02 21:26:59 +0200

    understanding if close

--- hello_world.rb
+++ hello_world.rb
@@ -1,5 +1,5 @@
-puts("Enter your first number");
-first_number = gets.chomp();
-puts("Enter your second number");
-second_number = gets.chomp();
-puts(first_number.to_f + second_number.to_f);
+puts('Enter your first number')
+first_number = gets.chomp
+puts('Enter your second number')
+second_number = gets.chomp
+puts(first_number.to_f + second_number.to_f)
--- madlin_game.rb
+++ madlin_game.rb
@@ -1,1 +1,10 @@
-if
+def max(num1, num2, num3)
+  return num1 if num1 >= num2 && num1 >= num3
+      
+   num2 >= num1 && num2 >= num3
+      num2
+  else
+      num3
+  
+end
+puts(max(100, 40, 50))
