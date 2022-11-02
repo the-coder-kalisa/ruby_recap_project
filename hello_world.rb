9dc0f29e19e895633ae1cd794fe3c5041741d47a
the-coder-kalisa <gthecoderkalisaineza@gmail.com>
2022-11-02 20:46:57 +0200

    finished calculator

--- hello_world.rb
+++ hello_world.rb
@@ -1,2 +1,5 @@
-phrase = "kalisa";
-puts phrase[0]
+puts("Enter your first number");
+first_number = gets.chomp();
+puts("Enter your second number");
+second_number = gets.chomp();
+puts(first_number.to_f + second_number.to_f);
