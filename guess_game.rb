8af14ae916fb6527b7f93affd6387c400adb8acb
the-coder-kalisa <gthecoderkalisaineza@gmail.com>
2022-11-03 07:54:13 +0200

    now it is okay

--- guess_game.rb
+++ guess_game.rb
@@ -28,6 +28,7 @@ guess_count = 0
 guess_limit = 3
 out_of_choices = false
 puts placeHolder
+puts "You have got " + guess_limit.to_s +  " chances"
 while guess != choosed[choice] && !out_of_choices
   if guess_count < guess_limit
     puts 'Enter guess: '
